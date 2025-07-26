45f282f479808cb0420a8e626e50cf74d2a4a5d8
Abdullah <abdullahxoff@gmail.com>
2025-07-26 20:08:49 +0300

    clear request for filter & search

--- job-app/resources/views/dashboard.blade.php
+++ job-app/resources/views/dashboard.blade.php
@@ -13,21 +13,40 @@
             <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-6">
                 <form action="{{ route('dashboard') }}" method="GET" class="flex w-full md:w-1/3">
                     <input name="search" value=" {{ request('search') }} " type="text" placeholder="Search for a job"
-                        class="w-full p-2.5 rounded-l-lg border border-gray-600 bg-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 text-sm text-white">
+                        class=" text-white w-full p-2.5 rounded-l-lg border border-gray-600 bg-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 text-sm">
                     <button type="submit"
                         class="bg-indigo-600 text-white px-4 py-2.5 rounded-r-lg hover:bg-indigo-700 transition">Search</button>
+
+                    @if (request('filter'))
+                        <input type="hidden" name="filter" value="{{ request('filter') }}">
+                    @endif
+
+                    @if (request('search'))
+                        <a href="{{ route('dashboard', ['filter' => request('filter')]) }}"
+                            class="text-indigo-300 px-4 py-2 rounded-lg hover:text-white transition">
+                            Clear</a>
+                    @endif
+
                 </form>
 
                 <div class="flex flex-wrap gap-2">
-                    <a href="{{ route('dashboard' , [ 'filter' => 'Full-Time' , 'search' => request('search') ]) }}" class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
+                    <a href="{{ route('dashboard', ['filter' => 'Full-Time', 'search' => request('search')]) }}"
+                        class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
                         Full-Time</a>
-                    <a href="{{ route('dashboard' , [ 'filter' => 'Remote' , 'search' => request('search') ]) }}" class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
+                    <a href="{{ route('dashboard', ['filter' => 'Remote', 'search' => request('search')]) }}"
+                        class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
                         Remote</a>
-                    <a href="{{ route('dashboard' , [ 'filter' => 'Hybrid' , 'search' => request('search') ]) }}" class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
+                    <a href="{{ route('dashboard', ['filter' => 'Hybrid', 'search' => request('search')]) }}"
+                        class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
                         Hybrid</a>
-                    <a href="{{ route('dashboard' , [ 'filter' => 'Contract' , 'search' => request('search') ]) }}" class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
+                    <a href="{{ route('dashboard', ['filter' => 'Contract', 'search' => request('search')]) }}"
+                        class="bg-indigo-700/20 text-indigo-300 px-4 py-2 rounded-lg hover:bg-indigo-700/30 transition">
                         Contract</a>
-                    <a class="text-gray-400 underline text-sm hover:text-white transition">Clear Filters</a>
+                    @if (request('filter'))
+                        <a href="{{ route('dashboard') }}"
+                            class="text-indigo-300 px-4 py-2 rounded-lg hover:text-white transition">
+                            Clear</a>
+                    @endif
                 </div>
             </div>
 
